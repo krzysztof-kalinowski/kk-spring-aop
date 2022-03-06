d7d61ce833b233eb242c56b7a760cf6ac09660b5
Krzysztof Kalinowski <kalinowski.krzysztof@gmail.com>
2022-03-06 20:51:03 +0100

    Using AOP to mesure method execution time

--- src/main/java/kkalinowski/springaop/aop/TimeLoggingAspect.java
+++ src/main/java/kkalinowski/springaop/aop/TimeLoggingAspect.java
@@ -1,4 +1,38 @@
 package kkalinowski.springaop.aop;
 
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@EnableAspectJAutoProxy
+@Component
+@Slf4j
 public class TimeLoggingAspect {
+
+    @Pointcut("execution(* kkalinowski.springaop..*(..))")
+    private void timeLogPointcut() {
+    }
+
+    @Before("timeLogPointcut()")
+    public void beforeTimeLogPointcut(JoinPoint joinPoint) {
+        log.info("beforeTimeLogPointcut "+ joinPoint.getSignature().getName());
+    }
+
+    @After("timeLogPointcut()")
+    public void afterTimeLogPointcut(JoinPoint joinPoint) {
+        log.info("afterTimeLogPointcut "+ joinPoint.getSignature().getName());
+    }
+
+    @Around("execution(* kkalinowski.springaop.controller..*(..))")
+    public Object aroundTimeLogPointCut(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        long startTime = System.nanoTime();
+        Object proceed = proceedingJoinPoint.proceed();
+        long time = System.nanoTime() - startTime;
+        log.info("aroundTimeLogPointCut "+proceedingJoinPoint.getSignature().getName()+" time: "+time+" ns");
+        return proceed;
+    }
 }
--- src/main/java/kkalinowski/springaop/controller/TestController.java
+++ src/main/java/kkalinowski/springaop/controller/TestController.java
@@ -10,7 +10,6 @@ public class TestController {
 
     @GetMapping("/time")
     public void timeTest() {
-        log.info("TestController.time");
     }
 
 
